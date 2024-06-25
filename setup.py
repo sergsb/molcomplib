990c8a34404b31a3fca5841f2089c94c4292093a
sergeys <sergey.sosnin@univie.ac.at>
2024-06-25 10:16:43 +0200

    package description is in order

--- setup.py
+++ setup.py
@@ -2,20 +2,30 @@ from setuptools import setup
 
 setup(
     # Your setup arguments
-    python_requires='>=3.6',  # Your supported Python ranges
-    name = "molcomplib",
-    version = "1.1.1",
-    description = "A python library and the standatrized model for the navigation in chemical space. It consists of parametric t-SNE model pretrained on large dataset of bioactive compounds"
-                  "This library is a part of MolCompass project. ",
-    author = "Sergey Sosnin <serg.sosnin@gmail.com>",
+    python_requires=">=3.6",  # Your supported Python ranges
+    name="molcomplib",
+    version="1.1.2",
+    description="A library for the projection of chemical compounds onto 2D space, mostly for visualization purposes.",
+    long_description="""Molcomplib is a key component of the MolCompass project. It is a python library, that provides the pretrained parametric t-SNE model for molecular visualization. This model was trained on ChEMBL data (about 1 Million of molecules). This library generates X and Y coordinates for compounds so that similar compounds group together forming well-recognisible clusters. The library is lightweight, and requires only rdkit and numpy (optionally, pandas)""",
+    author="Sergey Sosnin",
+    author_email="sergey.sosnin@univie.ac.at",
     include_package_data=True,
+    url="https://github.com/sergsb/molcompview",
     install_requires=[
-        'rdkit',
-        'numpy',
+        "rdkit",
+        "numpy",
     ],
     packages=["molcomplib"],
-    package_data={
-        "molcomplib": ["bin/*.pkl"]
-    },
-    license = "MIT",
+    package_data={"molcomplib": ["bin/*.pkl"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Intended Audience :: Science/Research",
+    ],
+    keywords="chemistry, cheminformatics, t-SNE, visualization,  chemical space",
+    license="MIT",
 )
