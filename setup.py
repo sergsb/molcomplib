2f996565523d6e04fc46c6b2e1ffac7aa6a5f301
Sergey Sosnin <sergey.sosnin@univie.ac.at>
2025-05-16 12:01:49 +0200

    updated PyPi pipeline

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     # Your setup arguments
     python_requires=">=3.6",  # Your supported Python ranges
     name="molcomplib",
-    version="1.1.2",
+    version="1.1.3",
     description="A library for the projection of chemical compounds onto 2D space, mostly for visualization purposes.",
     long_description="""Molcomplib is a key component of the MolCompass project. It is a python library, that provides the pretrained parametric t-SNE model for molecular visualization. This model was trained on ChEMBL data (about 1 Million of molecules). This library generates X and Y coordinates for compounds so that similar compounds group together forming well-recognisible clusters. The library is lightweight, and requires only rdkit and numpy (optionally, pandas)""",
     author="Sergey Sosnin",
